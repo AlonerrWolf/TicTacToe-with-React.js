84cd2b03b325d04b030c702d52cea681fda72cee
AlonerrWolf <74774676+AlonerrWolf@users.noreply.github.com>
2022-04-11 16:14:42 +0300

    Printing X on the board and lifting the state to board class

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Board extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        };
+    }
     renderSquare(i) {
-        return <Square value={i}/>
+        return <Square value={this.state.squares[i]}
+                       onClick={()=> this.handleClick(i)}
+        />;
+
+    }
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
     }
 
     render() {
@@ -32,12 +46,11 @@ class Board extends React.Component {
 }
 
 class Square extends React.Component {
+
     render() {
         return (
-            <button className={"square"} onClick={function () {
-                console.log('Click')
-
-            }}>
+            <button className="square" onClick={() => this.props.onClick()}
+            >
                 {this.props.value}
             </button>
         );
